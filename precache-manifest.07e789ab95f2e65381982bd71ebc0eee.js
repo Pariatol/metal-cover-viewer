080296ca33eaba0a71b4cf3f21a615e83950c886
Pariatol <pierre.tromeur@gmail.com>
2020-01-29 21:22:04 +0100

    Updates

---
+++ precache-manifest.07e789ab95f2e65381982bd71ebc0eee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445b6224ea575999f87070f0b2bf6eb7",
+    "url": "/metal-cover-viewer/index.html"
+  },
+  {
+    "revision": "48d38ffd097471745485",
+    "url": "/metal-cover-viewer/static/css/main.46a864b7.chunk.css"
+  },
+  {
+    "revision": "c60f59c0839a6f7ce367",
+    "url": "/metal-cover-viewer/static/js/2.eb3a5f84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/metal-cover-viewer/static/js/2.eb3a5f84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48d38ffd097471745485",
+    "url": "/metal-cover-viewer/static/js/main.5bc37ea1.chunk.js"
+  },
+  {
+    "revision": "08a49acf55583dffe1a5",
+    "url": "/metal-cover-viewer/static/js/runtime-main.8e1bccb0.js"
+  }
+]);
